4d8b15e48ade840a0f7237d8191e60a15faa405b
overflow-gitch <tarikbikhandafne@outlook.com>
2023-03-26 19:30:45 +0100

    classification on live sensor input.

--- platform-testing/sensor_csv_impulse/classify.py
+++ platform-testing/sensor_csv_impulse/classify.py
@@ -5,6 +5,8 @@ import signal
 import time
 from edge_impulse_linux.runner import ImpulseRunner
 import io
+from data_sampler import Sampler
+from sensors import LightSensor, MotionSensor, LoudnessSensor
 
 runner = None
 
@@ -17,7 +19,7 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def help():
-    print('python classify.py <path_to_model.eim> <path_to_features.txt>')
+    print('python classify.py <path_to_model.eim>')
 
 def main(argv):
     try:
@@ -31,26 +33,27 @@ def main(argv):
             help()
             sys.exit()
 
-    if len(args) <= 1:
+    if len(args) != 1:
         help()
         sys.exit(2)
 
     model = args[0]
 
+    #The commented code below is for handling csv files. 
 
-    features_file = io.open(args[1], 'r', encoding='utf8')
-    features = features_file.read()
-    features = features.strip().split("\n")
+    # features_file = io.open(args[1], 'r', encoding='utf8')
+    # features = features_file.read()
+    # features = features.strip().split("\n")
     
-    # Strips the header of csv file
-    if isinstance(features[0][0], str):
-        features.pop(0)
+    # # Strips the header of csv file
+    # if isinstance(features[0][0], str):
+    #     features.pop(0)
 
-    i = 0
-    while i < len(features):
-        features[i] = features[i].split(",")
-        features[i] = [int(j) for j in features[i][1:]]
-        i+=1
+    # i = 0
+    # while i < len(features):
+    #     features[i] = features[i].split(",")
+    #     features[i] = [int(j) for j in features[i][1:]]
+    #     i+=1
 
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -58,17 +61,22 @@ def main(argv):
 
     print('MODEL: ' + modelfile)
 
-
+    sampler = Sampler(
+        LightSensor(),
+        MotionSensor(16),
+        LoudnessSensor(adc_channel=0)
+    )
     runner = ImpulseRunner(modelfile)
     try:
         model_info = runner.init()
         print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
 
-        res = runner.classify(features[0])
-        print("classification:")
-        print(res["result"])
-        print("timing:")
-        print(res["timing"])
+        while True:
+            res = runner.classify(sampler.read_sensors())
+            print("classification:")
+            print(res["result"])
+            print("timing:")
+            print(res["timing"])
 
     finally:
         if (runner):
