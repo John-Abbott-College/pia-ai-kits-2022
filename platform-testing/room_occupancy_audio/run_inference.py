4b0352e92a422b5b8555c2d9e3aa29a139922726
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2023-05-02 12:14:03 -0400

    Run audio inference file

---
+++ platform-testing/room_occupancy_audio/run_inference.py
@@ -0,0 +1,79 @@
+# Copied from Edge Impulse Python SDK Exaple:
+# https://github.com/edgeimpulse/linux-sdk-python/blob/master/examples/audio/classify.py
+
+# Dependencies:
+# - Edge Impulse for Linux
+# - Edge Impulse Linux Python SDK
+# - pip install opencv-python
+# - Download model with Edge Impulse CLI too:
+#    - edge-impulse-linux-runner --download modelfile.eim
+
+# Usage:
+# - Run the script with -h argument for expected arguments
+# python3 run_inference.py -h
+
+
+import os
+import sys, getopt
+import signal
+from edge_impulse_linux.audio import AudioImpulseRunner
+
+runner = None
+
+def signal_handler(sig, frame):
+    print('Interrupted')
+    if (runner):
+        runner.stop()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def help():
+    print('python classify.py <path_to_model.eim> <audio_device_ID, optional>' )
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "h", ["--help"])
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            help()
+            sys.exit()
+
+    if len(args) == 0:
+        help()
+        sys.exit(2)
+
+    model = args[0]
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    modelfile = os.path.join(dir_path, model)
+
+    with AudioImpulseRunner(modelfile) as runner:
+        try:
+            model_info = runner.init()
+            labels = model_info['model_parameters']['labels']
+            print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+
+            #Let the library choose an audio interface suitable for this model, or pass device ID parameter to manually select a specific audio interface
+            selected_device_id = None
+            if len(args) >= 2:
+                selected_device_id=int(args[1])
+                print("Device ID "+ str(selected_device_id) + " has been provided as an argument.")
+
+            for res, audio in runner.classifier(device_id=selected_device_id):
+                print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+                for label in labels:
+                    score = res['result']['classification'][label]
+                    print('%s: %.2f\t' % (label, score), end='')
+                print('', flush=True)
+
+        finally:
+            if (runner):
+                runner.stop()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
